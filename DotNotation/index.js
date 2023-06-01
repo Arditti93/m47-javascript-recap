880a8c2e7b8e34957c0542d8acfc64c36b20c19f
Arditti93 <alex.arditti@hotmail.com>
2023-06-01 10:52:08 +0100

    added dot notation

---
+++ DotNotation/index.js
@@ -0,0 +1,40 @@
+console.log("hello my name is Alex")
+console.log(1993)
+console.log(true)
+console.log(null)
+console.log(undefined)
+
+console.log(["Audi", "Volvo", "Ford"])
+console.table(["Audi", "Volvo", "Ford"]);
+
+
+let x = 11
+if(x !== 10) { // !== not to equal too !===
+    console.error("erorr")
+} else {
+    console.log("yes x is equal to 10")
+}
+
+
+let firstName = "Ashleigh"
+console.log(firstName.toUpperCase())
+
+
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(7, 13);
+console.log(part)
+
+
+let text1 = "       Hello World!        ";
+console.log(text1)
+let result1 = text.trim();
+console.log(result1)
+
+let text2 = "Visit Microsoft!";
+console.log(text2)
+let result2 = text.replace("Microsoft", "W3Schools");
+console.log(result2) 
+
+console.log(Math.floor(Math.random() *10))
+
+console.log(Math.floor(Math.random() * 20))
